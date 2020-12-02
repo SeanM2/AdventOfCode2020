e00db316c954891cc61f272ea2f042d2dda16377
SeanM2 <64910873+SeanM2@users.noreply.github.com>
2020-12-02 19:23:28 +0000

    Add files via upload

---
+++ Day 1/main.cpp
@@ -0,0 +1,75 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> ReadFileInformation()
+{
+    ifstream myfile;
+    myfile.open("input.txt");
+
+    if (myfile.is_open())
+    {
+        std::string line;
+        std::vector<int> outputArray;
+
+        while(std::getline(myfile,line))
+        {
+            int a;            
+            a = std::atoi(line.c_str());
+            outputArray.push_back(a);
+            cout << outputArray.back() << "\n";
+        }        
+
+        myfile.close();
+
+        return outputArray;
+    }
+
+    return vector<int>(0);
+}
+
+int FindTheCorrectEntries(vector<int> _inputData)
+{
+    for (int i = 0; i < _inputData.size(); i++)
+    {
+        for (int j = 0; j < _inputData.size(); j++)
+        {
+            cout << "Index: " << i << ", " << j << "\n";
+            int _ans = _inputData[i] + _inputData[j];
+            if (_ans == 2020)
+            {
+                cout << " Answer Found at: " << i << ", " << j << "\n";
+                
+                return _inputData[i] * _inputData[j];
+            }            
+        }        
+    }
+
+    return 0;
+}
+
+int main()
+{
+    vector<int> inputData = ReadFileInformation();
+    cout << "Input Data Length: " << inputData.size() << "\n";
+    
+    cout << "First Value: " << inputData[0] << "\n";
+
+    int answer = FindTheCorrectEntries(inputData);
+    
+    if (answer != 0)
+    {
+        std::cout << "The Result is: " << answer << "\n";
+    }
+    else
+    {
+        cout << "No Matches Found :(" << "\n";
+    }
+
+    return 0;
+}
